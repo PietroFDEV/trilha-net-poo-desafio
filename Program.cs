0f3993bf0ba8107fb53b6a129ac72ec4981f4815
PietroFDEV <pi_favoreto@yahoo.com.br>
2024-01-20 17:20:42 -0300

    Desafio DIO

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Celular Nokia");
+Smartphone nokia = new Nokia("12", "22","4",260);
+nokia.Ligar();
+nokia.InstalarAplicativo("Facebook");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Celular Iphone");
+Smartphone iphone = new Iphone("X", "2","8",1000);
+iphone.Ligar();
+iphone.InstalarAplicativo("Facebook");
